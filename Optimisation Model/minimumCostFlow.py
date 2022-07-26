a39f3b3ba9e058e0a348afd14c2b51e7412a503a
murtuswami <harsharamachandran14@gmail.com>
2022-07-26 17:07:45 +0100

    Ignore html files

--- Optimisation Model/minimumCostFlow.py
+++ Optimisation Model/minimumCostFlow.py
@@ -1,8 +1,11 @@
+import math
 from processOverTime import processTripsOverTime
 from getAndProcessData import getAndProcessData
 import pandas as pd 
 import numpy as np
 import geopandas as gpd
+import pyproj 
+from math import radians, cos, sin, asin, sqrt
 
 """
 trips,bikeStations = getAndProcessData()
@@ -22,18 +25,65 @@ bikeStations.to_pickle("bikestations.pkl")
 
 """
 
-
+print()
 bikeStations = pd.read_pickle("bikestations.pkl")
 bikeStations = gpd.GeoDataFrame(bikeStations,crs="EPSG:4326",geometry=gpd.points_from_xy(bikeStations.lon, bikeStations.lat) )
-
+bikeStations = bikeStations.to_crs(crs = pyproj.CRS("EPSG:27700"))
 supply = bikeStations[ bikeStations['demand'] <0]
 demand = bikeStations [ bikeStations['demand'] > 0]
-edges = [] 
+startEdge = []
+startGeo = [] 
+endEdge = [] 
+endGeo = []
 print(supply)
 print(demand)
+
 def reassignId(x,row):
     x = (x,row)
 for index , row in supply.iterrows():
-    temp = bikeStations.copy()
-    temp['id'] = temp['id'].map()
-    print(temp)
+    for i , r in demand.iterrows():
+        if row['id'] != r['id']:
+            startEdge.append(row['id'])
+            startGeo.append(row.geometry)
+            endEdge.append(r['id'])
+            endGeo.append(r.geometry)
+
+
+#edgeids in form supply, demand 
+d = {'edgeStart' : startEdge,'startPoint': gpd.GeoSeries(startGeo),'edgeEnd' :endEdge,'endPoint':gpd.GeoSeries(endGeo)}
+edges = pd.DataFrame(data=d)
+x =   gpd.GeoSeries(edges['startPoint'],crs=pyproj.CRS("EPSG:27700"))
+y = gpd.GeoSeries(edges['endPoint'],crs= pyproj.CRS("EPSG:27700"))
+distances = x.distance(y,align=False)
+
+
+edges['weight'] = distances
+edges =  edges.drop("startPoint", axis=1)
+edges = edges.drop("endPoint", axis=1)
+print(edges)
+
+
+sourceEnds = []
+sourceStarts = [-1 for x in range(0,len(supply))]
+sourceDistances = [0 for x in range(0,len(supply))] 
+for index,row in supply.iterrows():
+    sourceEnds.append(row['id'])
+d = {'edgeStart':sourceStarts,'edgeEnd':sourceEnds,'weight':sourceDistances}
+sourceEdges = pd.DataFrame(data = d )
+edges = pd.concat([sourceEdges,edges])
+
+sinkStarts = [] 
+sinkEnds = [-2 for x in range (0 ,len(demand))] 
+sinkDistances = [0 for x in range(0,len(demand))]
+
+for index,row in demand.iterrows():
+    sinkStarts.append(row['id'])
+
+
+d = {'edgeStart':sinkStarts,'edgeEnd':sinkEnds,'weight':sinkDistances}
+
+
+sinkEdges = pd.DataFrame(data = d)
+
+edges = pd.concat([sinkEdges,edges])
+print(edges)
