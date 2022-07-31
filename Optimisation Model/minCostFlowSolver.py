6414911e7c3594565e90d09be626fee26d4b6c85
murtuswami <harsharamachandran14@gmail.com>
2022-07-31 12:18:42 +0100

    Finish first draft of pyomo min-cost-flow solver

--- Optimisation Model/minCostFlowSolver.py
+++ Optimisation Model/minCostFlowSolver.py
@@ -1,23 +1,54 @@
 import pyomo
+import pandas
+import pyomo.opt
+import pyomo.environ as pyo
+
 
 
 class MinCostFlow:
     """
-    nodes are a list of nodes 
-    edges are a 2d list of edges
-    net is the flow 
-    costs is the cost of sending flow along each edge,same index as edges
-    capacity is the maximum amount of flow to send along each edge 
+    nodes is a dataframe containing id as index and imbalance as column 
+    edges are a dataframe containing [start,end] as index with weights and capacities
+    Code was adapted from the following : 
+
+
+    https://github.com/Pyomo/PyomoGallery/blob/master/pandas_min_cost_flow/min_cost_flow.py
     """
-    def __init__(self,nodes,edges,costs,net):
-        self.nodes = nodes
-        self.edges = edges
-        self.costs = costs
-        self.capacity = capacity
-
+    def __init__(self,nodesDF,edgesDF):
+        self.edges_data = edgesDF
+        self.node_data = nodesDF
+        self.node_set = nodesDF.index.unique()
+        self.edge_set = edgesDF.index.unique()
     
-    def makeModel():
-        return 
+    def makeModel(self):
+        self.m = pyo.ConcreteModel()
 
-    def solve():
+        self.m.node_set = pyo.Set(initialize = self.node_set)
+        self.m.edge_set = pyo.Set(initialize = self.edge_set,dimen = 2 )
+        #Make rule that we cant send flow along an edge unless its X value is true 
+
+        self.m.X = pyo.Var(self.m.edge_set,domain=pyo.NonNegativeReals) 
+        self.m.Y = pyo.Var(self.m.edge_set,within = Binary )
+
+        #Objective rule 
+        def obj_rule(m):
+            return sum(m.Y[e] * self.edges_data.ix[e,'weight'] for e in self.edge_set) 
+        self.m.Obj = pyo.Objective(rule = obj_rule,sense = pyo.minimize)
+
+        #Flow Conservation 
+        def flow_conservation(m,n): # m is model, n is node id that we are conserving flow 
+            edges = self.edges_data.reset_index()
+            incoming = edges[edges.edgeEnd ==n ]['edgeStart']
+            outgoing = edges[edges.edgeStart == n]['edgeEnd']
+            return sum(m.X[(p,n)] for p in incoming) - sum(m.Y[(n,s)] for s in outgoing ) == self.node_data.ix[n,'imbalance']
+        self.m.FlowCons = pyo.Constraint(self.m.node_set,rule = flow_conservation)
+
+        def flow_along_active_edge(m,n):
+            # flow, X must be greater than or equal to than Y which decides whether the edge is active 
+            # lets say edge is not active, so Y = 0. We sent 1 unit of flow, so it wont work 
+            # lets say edge is active. Y = 1. minimum flow is 1. So  X>=Y it will send 
+            return self.m.X[n] >= self.m.Y[n]
+        self.m.activeEdges = pyo.Constraint(self.m.edge_set,rule=flow_along_active_edge)
+
+    def solve(self):
         return 
--- Optimisation Model/minimumCostFlow.py
+++ Optimisation Model/minimumCostFlow.py
@@ -37,15 +37,14 @@ startEdge = []
 startGeo = [] 
 endEdge = [] 
 endGeo = []
-
-demand['demand'] = demand.apply(lambda x : math.ceil(x['demand']),axis = 1 )
-supply['demand'] = supply.apply(lambda x : math.floor(x['demand']),axis = 1 )
+capacity = [] 
+#demand['demand'] = demand.apply(lambda x : math.ceil(x['demand']),axis = 1 )
+#supply['demand'] = supply.apply(lambda x : math.floor(x['demand']),axis = 1 )
 print(supply['demand'].sum())
 print(demand['demand'].sum())
-pdb.set_trace()
 
-def reassignId(x,row):
-    x = (x,row)
+#Supply -> Demand 
+
 for index , row in supply.iterrows():
     for i , r in demand.iterrows():
         if row['id'] != r['id']:
@@ -53,42 +52,51 @@ for index , row in supply.iterrows():
             startGeo.append(row.geometry)
             endEdge.append(r['id'])
             endGeo.append(r.geometry)
+            capacity.append(abs(row['demand']))
+
 
 
 #edgeids in form supply, demand 
-d = {'edgeStart' : startEdge,'startPoint': gpd.GeoSeries(startGeo),'edgeEnd' :endEdge,'endPoint':gpd.GeoSeries(endGeo)}
+d = {'edgeStart' : startEdge,'startPoint': gpd.GeoSeries(startGeo),'edgeEnd' :endEdge,'endPoint':gpd.GeoSeries(endGeo),'cap':capacity}
 edges = pd.DataFrame(data=d)
 x =   gpd.GeoSeries(edges['startPoint'],crs=pyproj.CRS("EPSG:27700"))
 y = gpd.GeoSeries(edges['endPoint'],crs= pyproj.CRS("EPSG:27700"))
 distances = x.distance(y,align=False)
-
-
 edges['weight'] = distances
 edges =  edges.drop("startPoint", axis=1)
 edges = edges.drop("endPoint", axis=1)
-edges['capacity'] = 1
+
 print(edges)
 
 
 sourceEnds = []
 sourceStarts = [-1 for x in range(0,len(supply))]
 sourceDistances = [0 for x in range(0,len(supply))] 
-#sourceCap = [ 2 for x ]
+
+sourceCap = []
+bikeStations = bikeStations.set_index('id')
+print(bikeStations)
 for index,row in supply.iterrows():
+    print(row['id'],row['demand'])
     sourceEnds.append(row['id'])
-d = {'edgeStart':sourceStarts,'edgeEnd':sourceEnds,'weight':sourceDistances}
+    sourceCap.append(abs(row['demand']))
+print(sourceCap)
+print(supply)
+
+d = {'edgeStart':sourceStarts,'edgeEnd':sourceEnds,'weight':sourceDistances,'cap':sourceCap}
 sourceEdges = pd.DataFrame(data = d )
 edges = pd.concat([sourceEdges,edges])
 
 sinkStarts = [] 
 sinkEnds = [-2 for x in range (0 ,len(demand))] 
 sinkDistances = [0 for x in range(0,len(demand))]
-
+sinkCap = []
 for index,row in demand.iterrows():
     sinkStarts.append(row['id'])
-
-
-d = {'edgeStart':sinkStarts,'edgeEnd':sinkEnds,'weight':sinkDistances}
+    sinkCap.append(row['demand'])
+print(sinkCap)
+ 
+d = {'edgeStart':sinkStarts,'edgeEnd':sinkEnds,'weight':sinkDistances,'cap':sinkCap}
 
 sinkEdges = pd.DataFrame(data = d)
 
@@ -98,19 +106,8 @@ edges = pd.concat([sinkEdges,edges])
 
 print(edges)
 nodes = pd.concat([demand,supply])
-print(demand)
-
 idSet = nodes['id'].to_list()
-demandSet = nodes['demand'].to_list()
-net = []
-for x in idSet:
-    if x == -1:
-        net.append(len(demand))
-    if x == -2:
-        net.append(-len(demand))
 print(idSet)
-print(demandSet)
-
 
 edgeId = []
 edgeWeight = []
@@ -119,7 +116,7 @@ edgeCap = []
 for index,row in edges.iterrows():
     edgeId.append((row['edgeStart'],row['edgeEnd']))
     edgeWeight.append(row['weight'])
-    edgeCap.append(row['capacity'])
+    edgeCap.append(row['cap'])
 print(edgeId)
 print(edgeWeight)
 print(edgeCap)
